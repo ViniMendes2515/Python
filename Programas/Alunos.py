138f9ceaca75a768025638b87db480886f7f65f5
Vinicius Mendes <vinimcastro@protonmail.com>
2022-11-04 09:28:46 -0300

    Update Alunos.py

--- Programas/Alunos.py
+++ Programas/Alunos.py
@@ -3,14 +3,13 @@ soma = 0
 
 
 for i in range(1, turma+1):
-    alunos = int(input("Digite o número de alunos da turma %i: "%i))
+    alunos = int(input(f"Digite o número de alunos da turma {i}:"))
     
     while alunos > 40 or alunos < 0:
         print("Número de alunos inválido")
         alunos = int(input("Digite o número de alunos da turma %i: "%i))
-    
-    
+
     soma += alunos
 
-print("A media de alunos por turma são: %g" %(soma/turma))
+print(f"A media de alunos por turma são: {soma/turma}")
 
